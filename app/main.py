61a90da0f474de20f592a1146dad575a347c4552
OrbitingOcelot <67998808+OrbitingOcelot@users.noreply.github.com>
2024-10-15 17:30:53 +0200

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ import email
 import re
 import time
 import os
+import logging
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
@@ -11,10 +12,15 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 EMAIL_IMAP = os.environ['EMAIL_IMAP']
 EMAIL_LOGIN = os.environ['EMAIL_LOGIN']
 EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
+EMAIL_POLLING_INTERVAL = os.environ['EMAIL_POLLING_INTERVAL']
 NETFLIX_EMAIL_SENDER = os.environ['NETFLIX_EMAIL_SENDER']
+
 
 
 def extract_links(text):
@@ -57,31 +63,40 @@ def open_link_with_selenium(body):
 def fetch_last_unseen_email():
     """Gets body of last unseen mail from inbox"""
     mail = imaplib.IMAP4_SSL(EMAIL_IMAP)
-    mail.login(EMAIL_LOGIN, EMAIL_PASSWORD)
-    mail.select("inbox")
+    
+    try:
+        mail.login(EMAIL_LOGIN, EMAIL_PASSWORD)
+        mail.select("inbox")
+    
+        _, email_ids = mail.search(None, '(UNSEEN FROM ' + NETFLIX_EMAIL_SENDER + ')')
+        email_ids = email_ids[0].split()
+    
+        if email_ids:
+            email_id = email_ids[-1]
+            _, msg_data = mail.fetch(email_id, "(RFC822)")
+            msg = email.message_from_bytes(msg_data[0][1])
+    
+            if msg.is_multipart():
+                for part in msg.walk():
+                    content_type = part.get_content_type()
+                    if "text/plain" in content_type:
+                        body = part.get_payload(decode=True).decode()
+                        open_link_with_selenium(body)
+            else:
+                body = msg.get_payload(decode=True).decode()
+                open_link_with_selenium(body)
 
-    _, email_ids = mail.search(None, '(UNSEEN FROM ' + NETFLIX_EMAIL_SENDER + ')')
-    email_ids = email_ids[0].split()
+    except Exception as e:
+        message = f"IMAP Error: {e}"
+        logger.error(message)
 
-    if email_ids:
-        email_id = email_ids[-1]
-        _, msg_data = mail.fetch(email_id, "(RFC822)")
-        msg = email.message_from_bytes(msg_data[0][1])
-
-        if msg.is_multipart():
-            for part in msg.walk():
-                content_type = part.get_content_type()
-                if "text/plain" in content_type:
-                    body = part.get_payload(decode=True).decode()
-                    open_link_with_selenium(body)
-        else:
-            body = msg.get_payload(decode=True).decode()
-            open_link_with_selenium(body)
-
-    mail.logout()
+    finally:
+        mail.logout()
 
 
 if __name__ == "__main__":
+    message = "Email monitoring starting"
+    logger.info(message)
     while True:
         fetch_last_unseen_email()
-        time.sleep(20)
+        time.sleep(EMAIL_POLLING_INTERVAL)
